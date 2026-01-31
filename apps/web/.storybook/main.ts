9b71a4e987fd58c05993d594ba8b7ab6adca2fe2
Grac11111aq <tadashi240312@gmail.com>
2026-01-31 15:09:11 +0900

    feat: add storybook setup for web

---
+++ apps/web/.storybook/main.ts
@@ -0,0 +1,31 @@
+import { mergeConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  viteFinal: async (config) =>
+    mergeConfig(config, {
+      plugins: [
+        viteTsConfigPaths({
+          projects: ['../tsconfig.json'],
+        }),
+        tailwindcss(),
+      ],
+    }),
+}
+
+export default config
